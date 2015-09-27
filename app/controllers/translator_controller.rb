5f6985b57a1ac67e7fdd60f5138a71f9bf9a1b0c
Riley Trautman <asonix.dev@gmail.com>
2015-09-27 09:44:01 -0500

    Fix translator

--- app/controllers/translator_controller.rb
+++ app/controllers/translator_controller.rb
@@ -1,33 +1,45 @@
 class TranslatorController < ApplicationController
+
+  skip_before_filter :verify_authenticity_token
+
   require 'json'
   require 'net/http'
   require 'uri'
+  require 'gyoku'
+  require 'nori'
 
-  def index
+  def post
     out_uri = URI.parse(get_destination)
 
     out_http = Net::HTTP.new(out_uri.host, out_uri.port)
-    out_request = Net::HTTP::Post.new(uri.request_uri)
 
-    out_request.body = get_hash.to_xml
+    if params[:req_type] == 'get'
+      out_request = Net::HTTP::Get.new(out_uri.request_uri)
+    elsif params[:req_type] == 'post'
+      out_request = Net::HTTP::Post.new(out_uri.request_uri)
+      out_request.body = Gyoku.xml(get_hash).to_s
+    end
 
-    request.headers.each do |key, value|
-      out_request[key] = value
+    unless request.headers[:authentication].blank?
+      out_request[:authentication] = request.headers[:authentication]
     end
 
     in_response = out_http.request(out_request)
 
-    render json: Hash.from_xml(in_response.body).to_json
+    parser = Nori.new
+    render json: parser.parse(in_response.body).to_json
   end
 
   private
 
   def get_destination
+    Rails.logger.info { "destination: #{params[:destination]}" }
     params[:destination]
   end
 
   def get_hash
-    params.select { |key, value| key != :destination }
+    Rails.logger.info { "site data: #{params[:site_data]}" }
+    params[:site_data]
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  post '/' => 'translator#index'
+  post '/' => 'translator#post'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
